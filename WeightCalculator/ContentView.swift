59d8a3c60312006dd3032ff2225762994f481a98
Vadim <vadim_yakimov@me.com>
2025-01-11 14:42:06 +0300

    Initial commit

---
+++ WeightCalculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeightCalculator
+//
+//  Created by Вадим on 11.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeightCalculator/WeightCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightCalculatorApp.swift
+//  WeightCalculator
+//
+//  Created by Вадим on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeightCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
